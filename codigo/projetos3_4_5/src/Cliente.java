16456e8bccc62a7e2184b29f2fa66099b62c469a
Laura_Melo <lauraenisiam@gmail.com>
2023-04-12 21:24:04 -0300

    criandoClasseCliente

---
+++ codigo/projetos3_4_5/src/Cliente.java
@@ -0,0 +1,30 @@
+public class Cliente {
+    private String nomeDeUsuario;
+    private String senha;
+    private Lista<Serie> listaParaVer;
+    private Lista<Serie> listaJaVistas;
+
+
+    public void adicionarNaLista(Serie serie){
+
+    }
+
+    public void retirarDaLista(String nomeSerie){
+        
+    }
+
+    public Lista<Serie> filtrarPorGenero(String genero){
+
+    }
+
+    public Lista<Serie> filtrarPorIdioma(String idioma){
+        
+    }
+
+    public Lista<Serie> filtrarPorQtdEpisodios(String genero){
+        
+    }
+
+
+
+}
