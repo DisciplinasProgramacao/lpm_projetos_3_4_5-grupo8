7488e96e473ea05f6a014bf15b930027197ef2e2
Laura_Melo <lauraenisiam@gmail.com>
2023-05-04 09:07:23 -0300

    #19 cadastrando filmes

--- codigo/projetos3_4_5/src/App.java
+++ codigo/projetos3_4_5/src/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class App {
     static Scanner teclado = new Scanner(System.in);
     static LinkedList<Catalogo> listaDeNovasSeries = new LinkedList<Catalogo>();
+    static LinkedList<Catalogo> listaDeNovosFilmes = new LinkedList<Catalogo>();
     static LinkedList<Cliente> listaDeClientes = new LinkedList<Cliente>();
     static Cliente ClienteTeste = new Cliente("Arthur", "arthur", "1234");
 
@@ -32,14 +33,34 @@ public class App {
                     System.out.println(plataforma.getCatalogo());
                     pausa();
                     break;
+                case 5:
+                    Catalogo filme = cadastrarFilme();
+                    plataforma.adicionarCatalogo(filme);
+                    System.out.println("Filme cradastrado com sucesso!");
+                    listaDeNovosFilmes.add(filme);
+                    pausa();
+                    break;
+                case 6:
+                    System.out.println("Exibindo filmes novos...");
+                    for (Catalogo catalogo : listaDeNovosFilmes) {
+                        System.out.println("#================================#");
+                        System.out.println(catalogo.toString());
+                    }
+                    pausa();
+                    break;
                 case 7:
+                    Serie.salvar("POO_Filmes_salvar", listaDeNovosFilmes);
+                    System.out.println("Filme cadastrado com sucesso!");
+                    pausa();
+                    break;
+                case 8:
                     Catalogo serie = criarSerie();
                     plataforma.adicionarCatalogo(serie);
                     System.out.println("Série criada com sucesso!");
                     listaDeNovasSeries.add(serie);
                     pausa();
                     break;
-                case 8:
+                case 9:
                     System.out.println("Exibindo séries novas...");
                     for (Catalogo catalogo : listaDeNovasSeries) {
                         System.out.println("#================================#");
@@ -47,16 +68,16 @@ public class App {
                     }
                     pausa();
                     break;
-                case 9:
+                case 10:
                     Serie.salvar("POO_Series_salvar", listaDeNovasSeries);
                     System.out.println("Serie salva com sucesso!");
                     pausa();
                     break;
-                case 10:
+                case 11:
                     cadastrarCliente(plataforma);
                     pausa();
                     break;
-                case 11:
+                case 12:
                     if(listaDeClientes.size() > 0){
                         plataforma.salvarClientes(listaDeClientes);
                     }else{
@@ -64,7 +85,7 @@ public class App {
                     }
                     pausa();
                     break;
-                case 12:
+                case 13:
                     avaliarCatalogo(ClienteTeste);
                     pausa();
                     break;
@@ -101,6 +122,20 @@ public class App {
         return new Serie(0, nome, dataLancamento, genero, idioma, quantidadeEpisodios);
     }
 
+    private static Filme cadastrarFilme() {
+        System.out.print("Por gentileza, digite o nome do filme: ");
+        String nome = teclado.nextLine();
+        System.out.print("Digite a data de lançamento: ");
+        String dataLancamento = teclado.nextLine();
+        System.out.print("Digite o Genero: ");
+        String genero = teclado.nextLine();
+        System.out.print("Digite o Idioma: ");
+        String idioma = teclado.nextLine();
+        System.out.print("Digite a duracao: ");
+        int duracao = Integer.parseInt(teclado.nextLine());
+        return new Filme(0, nome, dataLancamento, genero, idioma, duracao);
+    }
+
     public static int menuSites() {
         limparTela();
         System.out.println("Menu");
@@ -109,12 +144,15 @@ public class App {
         System.out.println("2 - Carregar Filmes");
         System.out.println("3 - Carregar Usuarios");
         System.out.println("4 - Exibir Catalagos");
-        System.out.println("7 - Criar Série");
-        System.out.println("8 - Exibir séries novas");
-        System.out.println("9 - Salvar Série");
-        System.out.println("10 - Cadastrar Cliente");
-        System.out.println("11 - Salvar Clientes");
-        System.out.println("12 - Avaliar Series e Filmes Vistos");
+        System.out.println("5 - Criar Filme");
+        System.out.println("6 - Exibir filmes novos");
+        System.out.println("7 - Salvar Filme");
+        System.out.println("8 - Criar Série");
+        System.out.println("9 - Exibir séries novas");
+        System.out.println("10 - Salvar Série");
+        System.out.println("11 - Cadastrar Cliente");
+        System.out.println("12 - Salvar Clientes");
+        System.out.println("13 - Avaliar Series e Filmes Vistos");
         System.out.println("==========================");
         System.out.print("\nDigite sua opção: ");
         int opcao = Integer.parseInt(teclado.nextLine());
